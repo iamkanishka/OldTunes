885b487fbf6f1ac33e2d553597d32bf260e0f54e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-05 21:28:21 +0530

    LyricsDisplayComponent init

---
+++ src/app/components/lyrics-display/lyrics-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lyrics-display',
+  templateUrl: './lyrics-display.component.html',
+  styleUrl: './lyrics-display.component.scss'
+})
+export class LyricsDisplayComponent {
+
+}
