18c8017b3d20f6279373effabc8259cea0203bfc
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-05 21:28:32 +0530

    SeekBarComponent init

---
+++ src/app/components/seek-bar/seek-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seek-bar',
+  imports: [],
+  templateUrl: './seek-bar.component.html',
+  styleUrl: './seek-bar.component.scss'
+})
+export class SeekBarComponent {
+
+}
