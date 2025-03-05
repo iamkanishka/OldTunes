2332cdfb38dbf59000bf35d4f21cb2788a95856e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-05 21:29:43 +0530

    NowPlayingRoutingModule init

---
+++ src/app/pages/now-playing/now-playing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NowPlayingRoutingModule } from './now-playing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NowPlayingRoutingModule
+  ]
+})
+export class NowPlayingModule { }
