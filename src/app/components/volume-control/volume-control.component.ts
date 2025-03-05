65d3b6e4035c161731e244a132d5d56003f2b6c8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-05 21:28:51 +0530

    VolumeControlComponent init

---
+++ src/app/components/volume-control/volume-control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-volume-control',
+  imports: [],
+  templateUrl: './volume-control.component.html',
+  styleUrl: './volume-control.component.scss'
+})
+export class VolumeControlComponent {
+
+}
