ea81a0dcb045e9047a4bb5b4de20dbbd0e708478
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-05 21:27:38 +0530

    ControlButtonsComponent init

---
+++ src/app/components/control-buttons/control-buttons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-buttons',
+  templateUrl: './control-buttons.component.html',
+  styleUrl: './control-buttons.component.scss'
+})
+export class ControlButtonsComponent {
+
+}
