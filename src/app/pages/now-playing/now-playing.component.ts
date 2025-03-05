4469a1b313802aad7d50b1a7f5c761312ee332d8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-05 21:29:38 +0530

    NowPlayingRoutingModule init

---
+++ src/app/pages/now-playing/now-playing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-now-playing',
+  imports: [],
+  templateUrl: './now-playing.component.html',
+  styleUrl: './now-playing.component.scss'
+})
+export class NowPlayingComponent {
+
+}
