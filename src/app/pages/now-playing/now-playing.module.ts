557c5fbf288688b47065ad32269e12fb4c6afecb
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-05 21:32:52 +0530

    chore: Update now-playing.module.ts to import NativeScriptCommonModule and add NO_ERRORS_SCHEMA

--- src/app/pages/now-playing/now-playing.module.ts
+++ src/app/pages/now-playing/now-playing.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptCommonModule } from '@nativescript/angular'
 import { NowPlayingRoutingModule } from './now-playing-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
+    NativeScriptCommonModule,
     NowPlayingRoutingModule
-  ]
+  ],
+  schemas: [NO_ERRORS_SCHEMA],
 })
 export class NowPlayingModule { }
